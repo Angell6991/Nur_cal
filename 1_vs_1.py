9a8dae1b3419f1b0271ebb723050de27e1a9b6c2
Angell6991 <miguel9196.guzman@gmail.com>
2025-07-29 23:33:17 -0500

    add InquirerPy_01

--- 1_vs_1.py
+++ 1_vs_1.py
@@ -11,7 +11,10 @@ os.system("clear")
 name_table_data =   "list_grups.xlsx"
 name_sheet_list =   pd.ExcelFile(str(name_table_data)).sheet_names
 
+
+######################################################################
 ###-----------------------Select_grup_01---------------------------###
+######################################################################
 select_grup_01  =   inp.select(
                         message="seletc grup 1: ",
                         choices=name_sheet_list
@@ -27,7 +30,10 @@ name_player_01  =   inp.select(
                         choices=list_players_01
                     ).execute()
 
+
+######################################################################
 ###-----------------------Select_grup_02---------------------------###
+######################################################################
 select_grup_02  =   inp.select(
                         message="seletc grup 2: ",
                         choices=name_sheet_list
@@ -43,7 +49,10 @@ name_player_02  =   inp.select(
                         choices=list_players_02
                     ).execute()
 
+
+######################################################################
 ###--------------------------Init_cal------------------------------###
+######################################################################
 player_01   =   np.array(tabla_01[f"{name_player_01}"])
 player_02   =   np.array(tabla_02[f"{name_player_02}"])
 
