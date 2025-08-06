cd05a6b066d8f94d6065c2e62258a631a66c9d3e
Angell6991 <miguel9196.guzman@gmail.com>
2025-08-05 20:33:01 -0500

    created edit_list_02, complete

--- edit_list.py
+++ edit_list.py
@@ -70,7 +70,6 @@ while   selection   !=  "Back":
                 atribut_list[4] =   int(input(" ** Dices: "))
                 data_list.append(atribut_list)
            
-            print("")
             data.new_list(direct, name_new_grup, name_list, data_list)
         
         os.system("clear")
@@ -100,13 +99,117 @@ while   selection   !=  "Back":
 
     ###-----------------------Edit_player------------------------------###
     elif  selection   ==  "Edit player":
-        
-        print("hola")
+       
+        os.system("clear")
+        list_directoriy =   os.listdir(direct)
+        list_directoriy =   [filename.replace(".dat", "") for filename in list_directoriy]
 
+        view_grup       =   inp.select(
+                                message="Select the group to edit: ",
+                                instruction="Press to Enter for select",
+                                choices=list_directoriy
+                            ).execute()
+        
+        edit_grup       =   pd.read_csv(f"{direct}/{view_grup}.dat", sep=r"\s+")
+        print(str("\n") + str(edit_grup.to_markdown(index=False))  + str("\n"))
+
+        select_player   =   inp.select(
+                                message="Select the player to edit: ",
+                                instruction="Press to Enter for select",
+                                choices=edit_grup.set_index("Player")
+                            ).execute()
+
+        select_atribut  =   inp.select(
+                                message="Select the player to edit: ",
+                                instruction="Press to Enter for select",
+                                choices=["Life", "Damage", "Dodge", "Attack", "Dices"]
+                                ).execute()
+
+        new_value       =   int(input(f"Introduc new {select_atribut}: "))
+
+        question        =   inp.confirm(
+                                message="Save changes?",
+                                default=True  
+                            ).execute()
+
+        if  question:
+            data.edit_player(direct, view_grup, select_player, select_atribut, new_value)
+
+            print(
+                str("\n") + 
+                str(pd.read_csv(f"{direct}/{view_grup}.dat", sep=r"\s+").to_markdown(index=False))  + 
+                str("\n")
+            )
+
+    ###---------------Add_new_player_in_grups--------------------------###
+    elif  selection   ==  "Add player":
+        
+        question        =   inp.confirm(
+                                message="Do you want to continue?",
+                                default=True  
+                            ).execute()
+
+        if  question:
+            
+            list_directoriy =   os.listdir(direct)
+            list_directoriy =   [filename.replace(".dat", "") for filename in list_directoriy]
+
+            view_grup       =   inp.select(
+                                    message="Select the group to edit: ",
+                                    instruction="Press to Enter for select",
+                                    choices=list_directoriy
+                                ).execute()
+
+            name_new_player     =   str(input("Enter the name of the new player: "))
+            atribut_list        =   []
+            list_name_atribut   =   ["Life", "Damage", "Dodge", "Attack", "Dices"]
+            
+            for i   in range(len(list_name_atribut)):
+                atribut_list.append(int(input(f" ** {list_name_atribut[i]}: ")))
+            
+            data.add_player(direct, view_grup, name_new_player, atribut_list)
+
+            print(
+                str("\n") + 
+                str(pd.read_csv(f"{direct}/{view_grup}.dat", sep=r"\s+").to_markdown(index=False))  + 
+                str("\n")
+            )
+
+    ###---------------Remove_players_in_grup---------------------------###
+    elif  selection   ==  "Remove player":
+
+        list_directoriy =   os.listdir(direct)
+        list_directoriy =   [filename.replace(".dat", "") for filename in list_directoriy]
+
+        view_grup       =   inp.select(
+                                    message="Select the group to edit: ",
+                                    instruction="Press to Enter for select",
+                                    choices=list_directoriy
+                                ).execute()
+
+        edit_grup       =   pd.read_csv(f"{direct}/{view_grup}.dat", sep=r"\s+")
+
+        player_delete   =   inp.checkbox(
+                                message="Select the group to edit: ",
+                                choices=edit_grup.set_index("Player"),
+                                instruction=" To select click space and to continue press Enter",
+                            ).execute()
+
+        question        =   inp.confirm(
+                                message="Do you want to continue delete players?",
+                                default=True  
+                            ).execute()
+
+        if  question:
+            for i   in  range(len(player_delete)):
+                data.remove_player(direct, view_grup, player_delete[i])
+
+            print(
+                str("\n") + 
+                str(pd.read_csv(f"{direct}/{view_grup}.dat", sep=r"\s+").to_markdown(index=False))  + 
+                str("\n")
+            )
 
 os.system("clear")
-
-
-
 
 
