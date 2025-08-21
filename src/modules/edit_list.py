06a5161abc0fb833b833ba8d0bec117ab5206710
Angell6991 <miguel9196.guzman@gmail.com>
2025-08-20 22:25:29 -0500

    created view dataframe 01

--- src/modules/edit_list.py
+++ src/modules/edit_list.py
@@ -6,54 +6,75 @@ import  os
 
 os.system("clear")
 
+######################################################################
+###--------------------export_contet_texting-----------------------###
+######################################################################
 direct_texting  =   os.path.join("..", "storage/data/list")
 contenido       =   os.listdir(direct_texting)
 
+######################################################################
+###-------------------definition_functions-------------------------###
+######################################################################
+
+###------------------------view_data_grups-------------------------###
 def view_table(direct, name):
 
     table   =   pd.read_csv(f"{direct}/{name}.dat", sep=r"\s+")
     columns =   table.columns.tolist()
 
     view    =   ft.DataTable(
-        columns =   [ft.DataColumn(ft.Text(str(col))) for col in columns],
+        
+        heading_row_color   =   "#121212",
+        sort_ascending      =   True,
+        bgcolor             =   "#e2e2e2",
+        border              =   ft.border.all(2, "#2e2e2e"),
+        border_radius       =   10,
+        vertical_lines      =   ft.border.BorderSide(1, "#2e2e2e"),
+        horizontal_lines    =   ft.border.BorderSide(1, "#2e2e2e"),
+        heading_row_height  =   35,
+        divider_thickness   =   0,
+        column_spacing      =   22,
+
+        columns =   [ft.DataColumn(ft.Text(str(col), color="#f4f4f4")) for col in columns],
         rows    =   [
             ft.DataRow(
-                [ft.DataCell(ft.Text(str(table.iloc[j,i]))) for i in range(len(columns))]
-            )
-            for j   in  range(5)
+                [ft.DataCell(ft.Text(str(table.iloc[j,i]), color="#2e2e2e"))    for i   in  range(len(columns))]
+            )   for j   in  range(5)
         ]
     )
 
     return  view
 
-######################################################################
-###----------------------------------------------------------------###
-######################################################################
-
-def edit_list(page: ft.Page):
-      
-    def action_boton(e):
-        return  page.add(ft.Text(str(contenido), size=30))
-
-    texto   =   ft.Text("hola mundo")
-    graph   =   view_table(direct_texting, "group texting")
+###----------------------button_crated_group-----------------------###
+def boton(action):
 
     boton   =   ft.FloatingActionButton(
-        on_click    =   action_boton, 
+        on_click    =   action, 
         icon        =   ft.Icons.ADD,
         bgcolor     =   "#2e2e2e",
         foreground_color    =   "#e2e2e2",
     )
+
+    return  boton
+
+
+def funcion(e):
+    return  print("hola mundo")
+
+######################################################################
+###-------------------------graph_in_flet--------------------------###
+######################################################################
+def edit_list(page: ft.Page):
+      
+    graph   =   view_table(direct_texting, "group texting")
    
-    page.padding = 20
-    page.scroll = ft.ScrollMode.HIDDEN
-    page.floating_action_button     =   boton
+    page.padding    =   20
+    page.scroll     =   ft.ScrollMode.HIDDEN
+    page.floating_action_button     =   boton(funcion)
 
-    return  page.add(texto, graph)
+    return  page.add(graph)
 
-######################################################################
 ###------------------------Texting_pogram--------------------------###
-######################################################################
 ft.app(target=edit_list)
 
 
@@ -101,7 +122,5 @@ ft.app(target=edit_list)
 #             padding=ft.padding.all(20),
 #         ),
 #     )
-
-
 # ft.app(main)
 
