99c21a728904e72cbafb4b080a68a27aca29f295
Angell6991 <miguel9196.guzman@gmail.com>
2025-08-22 16:53:50 -0500

    created main menu_04

--- src/modules/edit_list.py
+++ src/modules/edit_list.py
@@ -35,18 +35,16 @@ def view_table(direct, name):
         divider_thickness   =   0,
         column_spacing      =   22,
 
-        columns =   [ft.DataColumn(ft.Text(str(col), color="#f4f4f4")) for col in columns],
-        rows    =   [
-            ft.DataRow(
-                [ft.DataCell(ft.Text(str(table.iloc[j,i]), color="#2e2e2e"))    for i   in  range(len(columns))]
-            )   for j   in  range(5)
-        ]
+        columns =   [ft.DataColumn(ft.Text(str(col), color="#f4f4f4"))  for col in  columns],
+        rows    =   [ft.DataRow(
+            [ft.DataCell(ft.Text(str(table.iloc[j,i]), color="#2e2e2e"))    for i   in  range(len(columns))]
+        )   for j   in  range(5)]
     )
 
     return  view
 
 ###----------------------button_crated_group-----------------------###
-def boton(action):
+def button_new_group(action):
 
     boton   =   ft.FloatingActionButton(
         on_click    =   action, 
@@ -57,70 +55,93 @@ def boton(action):
 
     return  boton
 
+###-------------------------bar_intro_data-------------------------###
+def intro_bar(intro):
+    intro_text  =   ft.TextField(
+        label   =   str(intro), 
+        color   =   "#7b68ee", 
+        bgcolor =   "#e2e2e2", 
+        # on_change   =   save_bar
+        label_style =   ft.TextStyle(color="#808000"),
+        border_color    =   "#121212", 
+        border_radius   =   10,
+        border_width    =   1,
+        cursor_color    =   "#121212",
+        cursor_height   =   20,
+        cursor_radius   =   10,
+        cursor_width    =   1,
+        selection_color =   "#808000",
+    )
 
-def funcion(e):
-    return  print("hola mundo")
+    return  intro_text
+
+###-------------------------generit_button-------------------------###
+def generit_button(action, label):
+    boton   =   ft.FilledButton(
+        str(label),
+        on_click=   action,
+        color   =   "#121212",
+        bgcolor =   "#7b68ee",
+        style   =   ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10))
+    )
+    return  boton
+
 
 ######################################################################
 ###-------------------------graph_in_flet--------------------------###
 ######################################################################
 def edit_list(page: ft.Page):
-      
+
+    ###--------funcion_para_guardar_name_goups _y_number_players-------###
+    def new_list(e):
+
+        def save_and_exit(e):
+
+            page.controls.clear()
+            page.floating_action_button     =   button_new_group(new_list)
+            page.add(graph)
+            return  page.update()
+        
+        def save_players(e):
+
+            name    =   intro_bar("Name player")
+            life    =   intro_bar("Life")
+            damage  =   intro_bar("Damage")
+            dodge   =   intro_bar("Dodge")
+            attack  =   intro_bar("Attack")
+            dices   =   intro_bar("Dices")
+            button_save     =   ft.Row(
+                controls    =   [generit_button(save_and_exit, "Save list and exit"), 
+                                 generit_button(save_players, "Save data and next player")],
+                alignment   =   ft.MainAxisAlignment.CENTER,  
+            )
+
+            ###------------------------Dibujando_en_flet-----------------------###
+            page.controls.clear()
+            page.add(name, life, damage, dodge, attack, dices, button_save)
+            return  page.update()
+
+        name_group      =   intro_bar("Name group")
+        button_next     =   ft.Row(
+            controls    =   [generit_button(save_players, "Save name and intro players")],
+            alignment   =   ft.MainAxisAlignment.CENTER,  
+        )
+
+        ###------------------------Dibujando_en_flet-----------------------###
+        page.floating_action_button =   None
+        page.controls.clear()
+        page.add(name_group, button_next)
+        return page.update()
+
+    ###------------------------Dibujando_en_flet-----------------------###
     graph   =   view_table(direct_texting, "group texting")
-   
     page.padding    =   20
     page.scroll     =   ft.ScrollMode.HIDDEN
-    page.floating_action_button     =   boton(funcion)
-
-    return  page.add(graph)
+    page.floating_action_button     =   button_new_group(new_list)
+    
+    return  page.add(graph) 
 
 ###------------------------Texting_pogram--------------------------###
 ft.app(target=edit_list)
 
-
-
-
-
-# def main(page: ft.Page):
-#     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-#     page.padding = 20
-#     page.scroll = ft.ScrollMode.HIDDEN
-
-#     # keeps track of the number of tiles already added
-#     count = 1
-
-#     def fab_pressed(e):
-#         nonlocal count  # to modify the count variable found in the outer scope
-#         page.add(
-#             ft.ListTile(
-#                 title=ft.Text(f"Tile {count}"),
-#                 bgcolor=ft.Colors.TEAL_300,
-#                 leading=ft.Icon(
-#                     ft.Icons.CIRCLE_OUTLINED, color=ft.Colors.DEEP_ORANGE_300
-#                 ),
-#                 on_click=lambda x: print(x.control.title.value + " was clicked!"),
-#             )
-#         )
-#         count += 1
-
-#     page.floating_action_button = ft.FloatingActionButton(
-#         icon=ft.Icons.ADD, on_click=fab_pressed, bgcolor=ft.Colors.LIME_300
-#     )
-
-#     page.add(
-#         ft.Container(
-#             ft.Row(
-#                 [
-#                     ft.Text(
-#                         "Floating Action Button Example",
-#                         style=ft.TextStyle(size=20, weight=ft.FontWeight.W_500),
-#                     )
-#                 ],
-#                 alignment=ft.MainAxisAlignment.CENTER,
-#             ),
-#             bgcolor=ft.Colors.BLUE,
-#             padding=ft.padding.all(20),
-#         ),
-#     )
-# ft.app(main)
 
