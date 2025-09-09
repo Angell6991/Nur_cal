055f3d062e737aff54f61c6f10a2d01638794595
Angell6991 <miguel9196.guzman@gmail.com>
2025-09-09 17:11:54 -0500

    versus 04

--- src/modules/info_versus.py
+++ src/modules/info_versus.py
@@ -137,7 +137,26 @@ class   info_versus:
 
     ###------------------------view_result-----------------------------###
     def view_result(self, group_01, group_02, player_01, player_02, menu_navegation):
+        
+        ###--------------------------top_baner-----------------------------###
+        boton_back  =   ft.IconButton(
+            ft.Icons.ARROW_BACK_IOS_OUTLINED, 
+            bgcolor =   self.color[6],
+            icon_size   =   40,
+            on_click    =    lambda e:   self.main_menu(menu_navegation) 
+        )
+        texto_title =   ft.Text(
+            "Result probability",
+            color   =   self.color[6], 
+            weight  =   ft.FontWeight.BOLD, 
+            italic  =   True,
+            size    =   30,
+            font_family =   self.font[0],
+            text_align  =   ft.TextAlign.START,
+        )
+        top_baner   =   ft.Container(ft.Row([boton_back, texto_title], spacing=20))
 
+        ###---------------------import_data_player-------------------------###
         tabla_01    =   pd.read_csv(f"{self.direct_groups}/{group_01}.dat", sep=r"\s+")
         tabla_02    =   pd.read_csv(f"{self.direct_groups}/{group_02}.dat", sep=r"\s+")
 
@@ -145,23 +164,112 @@ class   info_versus:
         data_player_02  =   np.array(tabla_01[f"{player_02}"])
 
         pb  =   prob.probability(data_player_01, data_player_02)
-
+        
+        ###-------------craeted_graph_die_blows_probability----------------###
         list_die_01 =   pb.list_probability_die_1
         list_die_01 =   [round(list_die_01[i]*100, 1) for i in range(len(list_die_01))]
 
         list_die_02 =   pb.list_probability_die_2
         list_die_02 =   [round(list_die_02[i]*100, 1) for i in range(len(list_die_02))]
 
-
         graph_01    =   self.graph(group_01, player_01, pb.list_unique_1, list_die_01)
         graph_02    =   self.graph(group_02, player_02, pb.list_unique_2, list_die_02)
 
-        
+        ###---------------------container_view_result----------------------###
+        dat_01  =   ft.Container(ft.Column([graph_01]), border_radius=15, bgcolor=self.color[1])
+        dat_02  =   ft.Container(ft.Column([graph_02]), border_radius=15, bgcolor=self.color[1])
+
+
+        texto_01_r   =   [
+            ft.Text(
+                f"Probability attack true: {round(pb.probability_attack_1_true*100 ,1)}%",
+                font_family=self.font[1],
+                size=12,
+                color=self.color[6]
+            ),
+            ft.Text(
+                f"Probability relaunch: {round(pb.probability_attack_1_relaunch*100 ,1)}%",
+                font_family=self.font[1],
+                size=12,
+                color=self.color[6]
+            ),
+            ft.Text(
+                f"Probability win: {round((1-pb.probability_kill_1)*100 ,1)}%",
+                font_family=self.font[1],
+                size=12,
+                color=self.color[6]
+            ),
+        ]
+        texto_01_l   =   [
+            ft.Text(f"Life: {data_player_01[0]}", font_family=self.font[1], size=12, color=self.color[6]),
+            ft.Text(f"Damage: {data_player_01[1]}", font_family=self.font[1], size=12, color=self.color[6]),
+            ft.Text(f"Dodge: {data_player_01[2]}", font_family=self.font[1], size=12, color=self.color[6]),
+            ft.Text(f"Attack: {data_player_01[3]}", font_family=self.font[1], size=12, color=self.color[6]),
+            ft.Text(f"Dices: {data_player_01[4]}", font_family=self.font[1], size=12, color=self.color[6]),
+        ]
+        info_dat_01 =   ft.Container(
+            ft.Row(
+                [
+                    ft.Container(ft.Column(texto_01_l), border_radius=15, bgcolor=self.color[1], padding=10), 
+                    ft.Container(ft.Column(texto_01_r), border_radius=15, bgcolor=self.color[1], padding=10, expand=True)
+                ]
+            )
+        )
+
+
+        texto_02_r   =   [
+            ft.Text(
+                f"Probability attack true: {round(pb.probability_attack_2_true*100 ,1)}%",
+                font_family=self.font[1],
+                size=12,
+                color=self.color[6]
+            ),
+            ft.Text(
+                f"Probability relaunch: {round(pb.probability_attack_2_relaunch*100 ,1)}%",
+                font_family=self.font[1],
+                size=12,
+                color=self.color[6]
+            ),
+            ft.Text(
+                f"Probability win: {round((1-pb.probability_kill_2)*100 ,1)}%",
+                font_family=self.font[1],
+                size=12,
+                color=self.color[6]
+            ),
+        ]
+        texto_02_l   =   [
+            ft.Text(f"Life: {data_player_02[0]}", font_family=self.font[1], size=12, color=self.color[6]),
+            ft.Text(f"Damage: {data_player_02[1]}", font_family=self.font[1], size=12, color=self.color[6]),
+            ft.Text(f"Dodge: {data_player_02[2]}", font_family=self.font[1], size=12, color=self.color[6]),
+            ft.Text(f"Attack: {data_player_02[3]}", font_family=self.font[1], size=12, color=self.color[6]),
+            ft.Text(f"Dices: {data_player_02[4]}", font_family=self.font[1], size=12, color=self.color[6]),
+        ]
+        info_dat_02 =   ft.Container(
+            ft.Row(
+                [
+                    ft.Container(ft.Column(texto_02_l), border_radius=15, bgcolor=self.color[1], padding=10), 
+                    ft.Container(ft.Column(texto_02_r), border_radius=15, bgcolor=self.color[1], padding=10, expand=True)
+                ]
+            )
+        )
+
+
+        conte_infer =   ft.Container(
+            ft.Column(
+                [dat_01, info_dat_01, ft.Divider(color=self.color[0]), dat_02, info_dat_02], 
+                scroll=ft.ScrollMode.HIDDEN,
+                spacing=10,
+            ), 
+            height=600
+        )
+
+        ###------------------------main_container--------------------------###
+        cont    =   ft.Container(ft.Column([top_baner, ft.Divider(color=self.color[6]), conte_infer], spacing=10), padding=10)
+
         self.page.controls.clear()
-        self.page.add(graph_01, graph_02)
-        # self.page.add(cont, menu_navegation)
+        self.page.add(cont)
         # self.page.horizontal_alignment   =   ft.CrossAxisAlignment.CENTER
-        # self.page.vertical_alignment     =   ft.MainAxisAlignment.CENTER   
+        self.page.vertical_alignment     =   ft.MainAxisAlignment.START   
         return  self.page.update()
 
     ######################################################################
